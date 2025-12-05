e10cabadd44d151fdb5fa59b9a8bbd29011c5c22
elkulakova <elizaveta.kulakova@niuitmo.ru>
2025-12-05 13:40:44 +0300

    lab5 progress

--- lab5/2-tree_height.py
+++ lab5/2-tree_height.py
@@ -1,6 +1,24 @@
+def build_tree(parent):
+    n = len(parent)
+    children = [[] for _ in range(n)]
+    root = -1
+    for i in range(n):
+        if parent[i] == -1:
+            root = i
+        else:
+            children[parent[i]].append(i)
+    return root, children
+
+
+def get_tree_height(tree, n):
+    if tree is None:
+        return 0
+    for i in range(n-1):
+        tree = get_tree_height(tree, i + 1)
+        return [tree.index(leaf) for leaf in tree if leaf == i]
 
 
 if __name__ == "__main__":
     with open('input.txt') as f:
         n = int(f.readline())
-        tree = list(map(int, f.readline().split()))
+        array = list(map(int, f.readline().split()))
